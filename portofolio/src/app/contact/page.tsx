97d12f870113282bf916dd1e588bb5145f34aa0a
owais khilji <owaiskhilji6@gmail.com>
2024-10-01 09:33:42 -0700

    complte

--- portofolio/src/app/contact/page.tsx
+++ portofolio/src/app/contact/page.tsx
@@ -5,12 +5,20 @@ function page () {
     <div>
 <div className="contact-details bg-white m-8 p-6 rounded-lg border w-96 shadow-lg mt-8">
  <h2 className="text-2xl font-bold mb-4">Contact Details</h2>
-  <p className="mb-2">Email: <a href="mailto:owaiskhilji6@gmail.com" className="text-blue-500 hover:underline">owaiskhilji6@gmail.com</a></p>
-  <p className="mb-2">Phone: <a href="tel:03108037950" className="text-blue-500 hover:underline">03108037950</a></p>
-  <p className="mb-2">Address: Karachi,sindh,Pakistan</p>
-  <p className="mb-2">LinkedIn: <a href="https://www.linkedin.com/in/owais-khilji-8102b221a" target="_blank" className="text-blue-500 hover:underline">LinkedIn Profile</a></p>
-  <p className="mb-2">GitHub: <a href="https://github.com/owaiskhilji" target="_blank" className="text-blue-500 hover:underline">GitHub Profile</a></p>
-</div>
+ <div className=''>
+ <div className='border w-85 h-10 py-2 pl-2  hover:border-blue-500'>
+ <input  className='outline-none w-80' type="text"  placeholder='Name' />
+ </div>
+ <br />
+ <div className='border w-85 h-10 py-2 pl-2  hover:border-blue-500'>
+ <input  className='outline-none w-80' type="email"  placeholder='Email' />
+ </div>
+ <br />
+ <div className='border w-85 h-10 py-2 pl-2 hover:border-blue-500'>
+ <input  className='outline-none w-80' type="number"  placeholder='Phone Number' />
+ </div>
+ </div>
+ </div>
 
 
     </div>
